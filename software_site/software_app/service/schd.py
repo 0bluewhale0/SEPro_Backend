0441cd473d91502cfd72b3a9801be1ef10f88773
vitaminyoyoyo <1198015544@qq.com>
2023-06-17 05:52:27 +0800

    6.17零点debug debug信息改成了print

--- software_site/software_app/service/schd.py
+++ software_site/software_app/service/schd.py
@@ -270,13 +270,13 @@ class Scheduler:
                 request.pile_id = target_pile
                 # 将请求加入到目标充电桩的等待队列中
                 self.__pile_schedulers[target_pile].push_to_queue(request)
-                debug("[scheduler] request %d has been moved into queue of pile %d",
+                print("[scheduler] request %d has been moved into queue of pile %d",
                       request.request_id, request.pile_id)
 
-        skip_types = set()
+        # skip_types = set()
         if self.__scheduling_mode != SchedulingMode.NORMAL:  # 代表一定有充电桩坏了
             pile_type = self.__pile_schedulers[self.__broken_pile_id].get_type()
-            skip_types.add(pile_type)  # 跳过进行故障调度的充电桩
+            # skip_types.add(pile_type)  # 跳过进行故障调度的充电桩
             while len(self.__recovery_queue) > 0:  # 模拟暂停等候区叫号服务
                 target_pile = self.__find_fastest_spare_pile(pile_type)
                 if target_pile is None:  # 队列全满
@@ -284,18 +284,20 @@ class Scheduler:
                 request = self.__recovery_queue.pop(0)
                 request.fail_flag = False
                 request.pile_id = target_pile
+                self.__requests_map[request.request_id] = request
+                self.__requests_map[request.request_id].is_removed = False
                 self.__pile_schedulers[target_pile].push_to_queue(request)
-                debug("[recovery] request %d has been moved into queue of pile %d.",
+                print("[recovery] request %d has been moved into queue of pile %d.",
                       request.request_id,
                       target_pile)
                 if len(self.__recovery_queue) == 0:  # 故障队列调度完成
-                    debug("[recovery] recovery queue is empty now. resume scheduling.")
+                    print("[recovery] recovery queue is empty now. resume scheduling.")
                     self.__scheduling_mode = SchedulingMode.NORMAL
-                    skip_types.clear()  # 清空 恢复正常调度
+                    # skip_types.clear()  # 清空 恢复正常调度
             if len(self.__recovery_queue) == 0:  # 故障队列调度完成
-                debug("[recovery] recovery queue is empty now. resume scheduling.")
+                print("[recovery] recovery queue is empty now. resume scheduling.")
                 self.__scheduling_mode = SchedulingMode.NORMAL
-                skip_types.clear()  # 清空 恢复正常调度
+                # skip_types.clear()  # 清空 恢复正常调度
 
         # 对未被故障影响的充电桩进行调度
         schedule_on_type(PileType.CHARGE)
@@ -330,7 +332,7 @@ class Scheduler:
                             continue
                         is_completed = self.__check_if_completed(executing_request)
                     if is_completed:
-                        debug("[scheduler] request %d completed.", executing_request.request_id)
+                        print("[scheduler] request %d completed.", executing_request.request_id)
                         executing_request.is_removed = True
                         # self.end_request(executing_request.request_id)
             sleep(1)
@@ -354,27 +356,37 @@ class Scheduler:
                 pile_scheduler = self.__pile_schedulers[pile_id]
                 pile_scheduler.remove(request_id)
             else:
-                request.is_removed = False
+                self.__requests_map[request_id].is_removed = False
+                # request.is_removed = False
+
             if request.is_executing:
                 if not self.__check_if_completed(request):
-                    debug("[scheduler] request %d is cancelled while executing.",
-                          request.request_id)
+                    if request.fail_flag == False:
+                        print("[scheduler] request %d is cancelled while executing.",
+                                request.request_id)
                 # 触发结算流程生成详单
-                debug("[scheduler] request %d created an order.", request_id)
+                print("[scheduler] request %d created an order.", request_id)
+
+                #计算实际充电量
                 end_time=get_datetime_now()
                 power = NORMAL_PILE_POWER if request.request_type == PileType.CHARGE else FAST_CHARGE_PILE_POWER
                 real_amount = (end_time - request.begin_time).total_seconds() / 3600 * power
-                request.amount = Decimal(min(request.amount, real_amount))
+                real_amount = Decimal(min(request.amount, real_amount))
+
+                #针对充电桩故障的情况，需要重新排队并计算剩余电量
+                if request.fail_flag == True:
+                    request.amount =  Decimal(max(request.amount - real_amount,0))
+                    self.__requests_map[request_id].amount = request.amount
+                    
                 order: Order = create_order(request.request_type,
                                             request.pile_id,
                                             request.username,
-                                            request.amount,
+                                            real_amount,
                                             request.begin_time,
                                             end_time,
                                             returned_order=True)
-                # self.__cache[request.username] = order
             else:
-                debug("[scheduler] request %d is cancelled.", request_id)
+                print("[scheduler] request %d is cancelled.", request_id)
 
             # pile_scheduler 有空位 触发调度流程
             self.__try_schedule()
@@ -392,7 +404,7 @@ class Scheduler:
                 request.amount = amount
                 return
 
-            # 修改了模式
+        # 修改了模式
         self.end_request(request_id)  # 取消请求
         self.submit_request(request_type,  # 重新请求
                             request.username,
@@ -430,7 +442,7 @@ class Scheduler:
             self.__username_to_request_id[username] = request_id
             Scheduler.__push_queue(waiting_queue, request)
 
-            debug("[scheduler] request %d from user %s is submitted", request_id, username)
+            print("[scheduler] request %d from user %s is submitted", request_id, username)
 
             # 等待区更新 尝试调度
             self.__try_schedule()
@@ -469,7 +481,7 @@ class Scheduler:
 
     def brake(self, pile_id: int) -> None:
         with self.__check_lock:
-            debug("[recovery] pile %d is down.", pile_id)
+            print("[recovery] pile %d is down.", pile_id)
 
             self.__scheduling_mode = DEFAULT_RECOVERY_MODE
             self.__broken_pile_id = pile_id
@@ -477,6 +489,7 @@ class Scheduler:
             pile_scheduler.is_broken = True
             executing_request = pile_scheduler.get_executing_request()
             if executing_request is not None:
+                self.__requests_map[executing_request.request_id].is_removed = True
                 executing_request.is_removed = True
                 # self.end_request(executing_request.request_id)
 
@@ -484,7 +497,7 @@ class Scheduler:
                 case SchedulingMode.PRIORITY:
                     requests = pile_scheduler.fetch_and_clear(include_executing=True)
                     for request in requests:
-                        debug("[recovery] request %d has been moved to recovery queue.",
+                        print("[recovery] request %d has been moved to recovery queue.",
                               request.request_id)
                         request.pile_id = None
                         request.fail_flag = True
@@ -501,7 +514,7 @@ class Scheduler:
                         _requests = _scheduler.fetch_and_clear(include_executing)
                         requests += _requests
                     for request in requests:
-                        debug("[recovery] request %d has been moved to recovery queue.",
+                        print("[recovery] request %d has been moved to recovery queue.",
                               request.request_id)
                         request.pile_id = None
                         request.fail_flag = True
@@ -511,12 +524,12 @@ class Scheduler:
 
     def recover(self, pile_id: int) -> None:
         with self.__check_lock:
-            debug("[recovery] pile %d is up.", pile_id)
+            print("[recovery] pile %d is up.", pile_id)
 
             self.__scheduling_mode = SchedulingMode.RECOVERY
             pile_scheduler = self.__pile_schedulers[pile_id]
             pile_scheduler.is_broken = False
-
+            self.__broken_pile_id = pile_id
             pile_type = pile_scheduler.get_type()
             requests: List[_ChargingRequest] = []
             for _, _scheduler in self.__pile_schedulers.items():
@@ -525,12 +538,14 @@ class Scheduler:
                 _requests = _scheduler.fetch_and_clear(include_executing=False)
                 requests += _requests
             for request in requests:
-                debug("[recovery] request %d has been moved to recovery queue.",
+                print("[recovery] request %d has been moved to recovery queue.",
                       request.request_id)
                 request.pile_id = None
                 request.fail_flag = True
             requests = sorted(requests)
             self.__recovery_queue = list(requests)
+
+        self.__try_schedule()
 
     def get_request_id_by_username(self, username: str) -> int:
         with self.__lock:
@@ -550,6 +565,7 @@ class Scheduler:
                 'waitingTime': (get_datetime_now() - request.create_time).seconds
             }
             request_list.append(request_info)
+        # breakpoint()
         return request_list
 
     def query_left_amount(self, request_id) -> Decimal:
